ea83df0974c91739fd5c062bb4247ac90edc1ed0
tpltnt <tpltnt.github@dropcut.net>
2013-03-20 12:27:37 +0100

    print 2to3, syntax error fixed

--- examples/mbus-test-1.py
+++ examples/mbus-test-1.py
@@ -16,25 +16,25 @@ address = 1
 mbus = Mbus("localhost", 1200)
 
 if debug:
-    print "mbus =", mbus
+    print("mbus = ", mbus)
 
 mbus.connect()
 
 if debug:
-    print "mbus =", mbus
+    print("mbus = ", mbus)
 
 mbus.send_request_frame(address)
 
 reply = mbus.recv_frame()
 
 if debug:
-    print "reply =", reply
+    print("reply =", reply)
 
 reply_data = mbus.frame_data_parse(reply)
 
 if debug:
-    print "reply_data =", reply_data
+    print("reply_data =", reply_data)
 
-xml_buff = mbus.frame_data_xmlreply_data)
+xml_buff = mbus.frame_data_xml(reply_data)
 
-print "xml_buff =", xml_buff
+print("xml_buff =", xml_buff)
