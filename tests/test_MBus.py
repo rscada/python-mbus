485a54d6b47f95617a128890375529938a8e7d4b
tpltnt <tpltnt.github@dropcut.net>
2013-04-03 21:38:07 +0200

    tests updated

--- tests/test_MBus.py
+++ tests/test_MBus.py
@@ -11,20 +11,20 @@ def test_empty_init():
 
 def test_device_null():
     with pytest.raises(TypeError):
-        foo = MBus.MBus('/dev/null')
+        foo = MBus.MBus(device='/dev/null')
 
 
 def test_device_nonexistent():
     with pytest.raises(FileNotFoundError):
-        foo = MBus.MBus('/dev/idonotexist')
+        foo = MBus.MBus(device='/dev/idonotexist')
 
 
 #def test_device_ttyUSB0():
 #    with pytest.raises(TypeError):
-#        foo = MBus.MBus('/dev/ttyUSB0')
+#        foo = MBus.MBus(device='/dev/ttyUSB0')
 # device=None, host=None, port=8888
 
 
 def test_device_and_host():
     with pytest.raises(BaseException):
-        foo = MBus.MBus('/dev/null', '127.0.0.1')
+        foo = MBus.MBus(device='/dev/null', host='127.0.0.1')
