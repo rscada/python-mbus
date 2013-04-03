14441f0b38299eb868503859d285498bdfbfb9fe
tpltnt <tpltnt.github@dropcut.net>
2013-04-03 18:16:55 +0200

    unused libmbus removed

--- mbus/MBusFrameData.py
+++ mbus/MBusFrameData.py
@@ -1,15 +1,3 @@
-from ctypes import *
-
-libmbus = None
-try:
-    libmbus = cdll.LoadLibrary('libmbus.so')
-except OSError:
-    libmbus = cdll.LoadLibrary('/usr/local/lib/libmbus.so')
-
-if None == libmbus:
-    raise OSError("libmbus not found")
-
-
 class MBusFrameData(Structure):
     _fields_ = [("data_var",   c_uint8 * 16),  # MBusFrameFixed
                 ("data_fixed", c_uint8),
