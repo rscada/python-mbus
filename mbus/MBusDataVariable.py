2ff123cabe724ea92b2b3da19b518d7e714b4f72
Stuart Longland <me@vk4msl.yi.org>
2015-12-09 11:53:42 +1000

    Add more enumerations.

--- mbus/MBusDataVariable.py
+++ mbus/MBusDataVariable.py
@@ -2,6 +2,7 @@ from ctypes import Structure,c_uint8,c_void_p,c_size_t,c_char_p
 
 from mbus.MBusDataVariableHeader import MBusDataVariableHeader
 
+
 class MBusDataVariable(Structure):
     _fields_ = [("header", MBusDataVariableHeader),
                 ("record", c_void_p),
--- mbus/MBusFrameData.py
+++ mbus/MBusFrameData.py
@@ -1,15 +1,36 @@
 from ctypes import Structure,c_uint8,c_uint32
+from enum import Enum
 
 from mbus.MBusDataVariable import MBusDataVariable
 from mbus.MBusDataFixed import MBusDataFixed
 
+
+class MBusFrameDataType(Enum):
+    '''
+    ABSTRACT DATA FORMAT (error, fixed or variable length)
+    '''
+    Fixed =    1
+    Variable = 2
+    Error =    3
+
+
+class MBusFrameError(Enum):
+    '''
+    GENERAL APPLICATION ERRORS
+    '''
+    Unspecified =     0x00
+    UnimplementedCI = 0x01
+    BufferTooLong =   0x02
+    TooManyRecords =  0x03
+    PrematureEnd =    0x04
+    TooManyDIFes =    0x05
+    TooManyVIFes =    0x06
+    Reserved =        0x07
+    ApplicationBusy = 0x08
+    TooManyReadouts = 0x09
+
+
 class MBusFrameData(Structure):
-    #
-    # ABSTRACT DATA FORMAT (error, fixed or variable length)
-    #
-    MBUS_DATA_TYPE_FIXED =    1
-    MBUS_DATA_TYPE_VARIABLE = 2
-    MBUS_DATA_TYPE_ERROR =    3
 
     _fields_ = [("data_var",   MBusDataVariable),
                 ("data_fixed", MBusDataFixed),
